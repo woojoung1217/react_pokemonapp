7aeaf7ed114616408e81b2f84cf7554b77f96b8f
woojoung <ywj981217@naver.com>
2023-12-10 19:00:59 +0900

    return

--- src/components/DamageModal.jsx
+++ src/components/DamageModal.jsx
@@ -13,7 +13,7 @@ const DamageModal = ({ setIsModalOpen, damages }) => {
 
   return (
     <div className="flex items-center justify-center z-40 fixed left-0 bottom-0 w-full h-full bg-gray-800">
-      <div ref={ref} className="modal bg-white rounded-lg w-1/2 py-2">
+      <div ref={ref} className="modal bg-white rounded-lg w-1/2 py-2 ">
         <div className="flex flex-col items-start p-4">
           <div className="flex items-center w-full justify-between">
             <div className="text-gray-900 font-medium text text-lg">
